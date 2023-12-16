0909bc584bc0c753f65755515224f57fe464cf63
sudhanvelss <sudhanssudhan83@gmail.com>
2023-12-16 20:25:45 +0530

    mission1 completed

--- Script.js
+++ Script.js
@@ -4,10 +4,66 @@ let SearchButton = document.querySelector('.search-btn') // Changed from getElem
 let url = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 
 
-let userInp = document.getElementById('input-box').value;
-
-fetch(url + 'big mac') 
+SearchButton.addEventListener('click', ()=> {
+    let userInp = document.getElementById('input-box').value;
+    if(userInp.length == 0){
+        Result.innerHTML = `<h3>Input Field cannot be Empty</h3>`
+    }
+    else{
+        fetch(url + userInp) 
     .then((response) => response.json())
     .then((data) => {
         console.log(data)
+        let myMeal = data.meals[0];
+        let count = 1
+        let ingredients = []
+        for (let i in myMeal){
+            let ingredient = "";
+            let measure = "";
+            if(i.startsWith('strIngredient') && myMeal[i]){
+                ingredient = myMeal[i]
+                measure = myMeal[`strMeasure` + count]
+                count += 1
+                ingredients.push(`${measure} ${ingredient}`)
+            }
+        } 
+        console.log(ingredients)
+
+        Result.innerHTML = `<img src = ${myMeal.strMealThumb}>
+        <div class= "details">
+            <h2>${myMeal.strMeal}</h2>
+            <h4>${myMeal.strArea}</h4>
+            </div>
+            <div id ="ingredient-con"></div>
+            <div id ="recipe">
+                <button id="hide-recipe">x</button>
+                <pre id= "instructions">${myMeal.strInstructions}</pre>
+            </div>
+            <button id ="show-recipe">View Recipe</button>
+            `
+
+            let ingredientCon = document.getElementById('ingredient-con')
+            let parent = document.createElement('ul')
+            let recipe = document.getElementById('recipe')
+            let hideRecipe = document.getElementById('hide-recipe')
+            let showRecipe = document.getElementById('show-recipe')
+
+            ingredients.forEach((i) => {
+                let child = document.createElement("li")
+                child.innerText = i
+                parent.appendChild(child)
+                ingredientCon.appendChild(parent)
+            })
+
+            hideRecipe.addEventListener('click', () => {
+                recipe.style.display = "none"
+            })
+            showRecipe.addEventListener('click', () => {
+                recipe.style.display = "grid"
+            })
+    }).catch(() => {
+        Result.innerHTML = `<h3>Invalid Input</h3>`
     })
+    }
+})
+
